4db65b1181e94ff1f91b6614d62b89c41be9c410
temi <fagunwaayomide59@gmail.com>
2026-07-02 22:49:28 +0100

    First upload:My To-Do List App

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const taskInput = document.getElementById('task-input');
+    const addTaskBtn = document.getElementById('add-task-btn');
+    const taskList = document.getElementById('task-list');
+    const emptyImage = document.querySelector('.empty-image');
+
+    const toggleEmptyState = () => {
+        emptyImage.style.display = taskList.children.length === 0 ? 'flex' : 'none';
+    };
+
+    const addTask = (event) => {
+        event.preventDefault();
+        const taskText = taskInput.value.trim();
+        if (!taskText) {
+            return;
+        }
+        const li = document.createElement('li');
+        li.innerHTML = `
+        <input type="checkbox" class="checkbox">
+        <span>${taskText}</span>
+        <div class="task-buttons">
+        <button class="edit-btn"><i class="fa-solid fa-pen"></i></button>
+        `;
+
+        taskList.appendChild(li);
+        taskInput.value = '';
+        toggleEmptyState();
+    };
+
+    addTaskBtn.addEventListener('click', addTask);
+    taskInput.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+            addTask(e);
+        }
+    });
+    toggleEmptyState();
+});
